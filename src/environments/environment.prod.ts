0647c8d8c3a0d53a3e945f3407dc6c681ed89957
BoZhaoUTSC <bodotzhao@gmail.com>
2018-01-08 16:13:25 -0500

    Initial Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiURL: "http://backend-demo-prod.127.0.0.1.nip.io"
+};
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiURL: "http://backend-demo-qa.127.0.0.1.nip.io"
+  };
+  
