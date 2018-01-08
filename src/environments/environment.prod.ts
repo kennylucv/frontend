e586ecb7a99fdc9bc398f6219d91fef6ccc2f679
BoZhaoUTSC <bodotzhao@gmail.com>
2018-01-08 16:33:12 -0500

    fixed endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: "http://backend-prod-environment.127.0.0.1.nip.io"
+  apiURL: "http://backend-http-route-production-environment.127.0.0.1.nip.io"
 };
