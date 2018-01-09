9f2741689d8f93f8a749806db36f152978a58ad1
BoZhaoUTSC <bodotzhao@gmail.com>
2018-01-09 13:30:41 -0500

    Change endpoint

--- test-frontend.py
+++ test-frontend.py
@@ -6,5 +6,5 @@ from selenium import webdriver
 for i in range(6):
     driver = webdriver.Chrome()
     # open browser and go to Google.ca
-    driver.get('http://frontend-http-route-dev-env.127.0.0.1.nip.io/login?returnUrl=%2F')
+    driver.get('http://frontend-prod-env.127.0.0.1.nip.io')
 
